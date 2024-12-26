c4735acc5ae0d94f6616cd9f26a803b3d1908761
emreceran <emrecrn@gmail.com>
2024-12-26 17:08:12 +0300

    3

--- models/models.py
+++ models/models.py
@@ -4,8 +4,8 @@
 from odoo import models, fields
 from odoo.tools.translate import html_translate
 
-# class CustomResUsers(models.Model):
-#     _inherit = 'res.users'
+class CustomResUsers(models.Model):
+    _inherit = 'res.users'
 
-#     profilePage = fields.Char(string="Profil", default="-")
-#     linkedin = fields.Char(string="Linkedin", default="-")
+    profilePage = fields.Char(string="Profil", default="-")
+    linkedin = fields.Char(string="Linkedin", default="-")
